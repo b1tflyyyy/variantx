6ec676f5cc6bb3ea4d79123b076d36e7131bb5a4
DADA30000 <vadimhack.ru@gmail.com>
2026-02-14 00:06:33 +0300

    write complete ideal variant

--- cmake/UnitTestsOptions.cmake
+++
@@ -1,6 +0,0 @@
-if (WIN32)
-    message(STATUS "OS: Windows")
-    set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-elseif (UNIX)
-    message(STATUS "OS: UNIX-Like")
-endif()
--- explanation/explanation.md
+++
@@ -1,4 +0,0 @@
-## Explanation
-
-I wrote a simple explanation of the core functions used in the `Visit` implementation.
-You can see it in `visit-core` folder.
